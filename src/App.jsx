378cc86259ff64483e4bf77c626d401a5a88df0f
Anastacy Slyusar <ana.milpalm@gmail.com>
2024-11-11 14:13:00 +0100

    The SearchForm have been added

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,48 @@ import axios from 'axios';
 import './App.css'
 import { ArticleList } from './components/ArticleList/ArticleList';
 import { fetchArticlesWithTopic } from './articles-api';
+import { SearchForm } from './components/SearchForm/SearchForm';
 
-function App() {
+const App = () => {
       const [articles, setArticles] = useState([]);
       const [loading, setLoading] = useState(false);
-      const [error, setError] = useState(false);
+     const [error, setError] = useState(false);
 
-  useEffect(() => {
-    async function fetchArticles() {
+
+
+  // useEffect(() => {
+  //   async function fetchArticles() {
+  //     try {
+  //       setLoading(true);
+  //       const data = await fetchArticlesWithTopic("react");
+  //       setArticles(data);
+  //     } catch (error) {
+  //       setError(true);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   }
+  //   fetchArticles();
+  // }, []);
+
+    const handleSearch = async (topic) => {
       try {
+        setArticles([]);
+        setError(false);
         setLoading(true);
-        const data = await fetchArticlesWithTopic("react");
+        const data = await fetchArticlesWithTopic(topic);
         setArticles(data);
       } catch (error) {
         setError(true);
       } finally {
         setLoading(false);
       }
-    }
-    fetchArticles();
-  }, []);
+    };
 
   return (
     <div>
-      <h1>Latest articles</h1>
+
+      <SearchForm onSearch={handleSearch} />
       {loading && <p style={{ fontSize:20 }}>Loading data, please wait...</p>}
       {error && (
         <p>Whoops, something went wrong! Please try reloading this page!</p>
